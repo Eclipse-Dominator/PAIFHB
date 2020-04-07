73de81fca4d0c7bb17dd2034c9db1d5a30202a35
WHK <weehekun@gmail.com>
2020-04-07 23:31:53 +0800

    e

--- src/app/services/lesson-data.service.ts
+++ src/app/services/lesson-data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { HTTP } from '@ionic-native/http/ngx/'; // mobile
+// import { HTTP } from '@ionic-native/http/ngx/'; // mobile
 
 // Handles data state and storage
 
