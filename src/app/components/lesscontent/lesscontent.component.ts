05d8cc51198de35a12a72919309719cbb1554e73
WHK <weehekun@gmail.com>
2020-04-08 18:46:37 +0800

    Inner html does not work
    yay

--- src/app/components/lesscontent/lesscontent.component.ts
+++ src/app/components/lesscontent/lesscontent.component.ts
@@ -9,16 +9,27 @@ import { LessonDataService, LessonData, LessonItem } from '../../services/lesson
 })
 export class LesscontentComponent implements OnInit {
     content: string;
+    content2: string = "<ion-text color='danger'>i am a butt</ion-text>";
+    procContent: string[];
     
     constructor(
         private dataSvce: LessonDataService
     ) { }
 
   ngOnInit() {
-    this.dataSvce.getSelectedContent().subscribe(
-      x => this.content = x,
-      err => this.content = "not found"
-    )
+      this.dataSvce.getSelectedContent().subscribe(
+          x => {
+              this.content = x;
+              this.procContent = this.content.split('\n');
+              for (let i = 0; i < this.procContent.length; i++) {
+                  let line: string = this.procContent[i];
+                  this.procContent[i] = line + "<br/>";
+              }
+              this.content = this.procContent.join('');
+          },
+        err => this.content = "not found"
+      )
+      
   }
 
 }
