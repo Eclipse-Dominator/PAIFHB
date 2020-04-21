fee015f8773a4f9f5dd47a762e198ad2de52d877
WHK <weehekun@gmail.com>
2020-04-21 22:31:19 +0800

    buggy stuff
    e

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 import { NavController } from "@ionic/angular";
+import { UserDataService } from "../services/user-data.service";
 
 import * as firebase from "firebase";
+import { UserInfo } from "firebase";
 
 @Component({
   selector: "app-login",
@@ -9,37 +11,47 @@ import * as firebase from "firebase";
   styleUrls: ["./login.page.scss"],
 })
 export class LoginPage implements OnInit {
-  constructor(private navCtrl: NavController) {}
+  constructor(
+    private navCtrl: NavController,
+    private uDataSvce: UserDataService
+  ) {}
 
-  user;
-  token;
+  user: UserInfo;
+  token: string;
 
   doGoogleLogin() {
     let provider = new firebase.auth.GoogleAuthProvider();
     firebase
       .auth()
-      .signInWithPopup(provider)
-      .then(
-        (result: any) => {
-          // This gives you a Google Access Token. You can use it to access the Google API.
-          this.token = result.credential.accessToken;
-          // The signed-in user info.
-          this.user = result.user;
-          // ...
-          console.log(this.user);
-        },
-        (error) => {
-          // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          // The email of the user's account used.
-          var email = error.email;
-          // The firebase.auth.AuthCredential type that was used.
-          var credential = error.credential;
-          // ...
-          console.log(errorMessage);
-        }
-      );
+      .setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+      .then(() => {
+        firebase
+          .auth()
+          .signInWithPopup(provider)
+          .then(
+            (result: any) => {
+              // This gives you a Google Access Token. You can use it to access the Google API.
+              this.token = result.credential.accessToken;
+              // The signed-in user info.
+              this.user = result.user;
+              // ...
+              // console.log(this.user);
+              this.uDataSvce.setUser(this.user);
+              this.uDataSvce.setSession(this.token);
+            },
+            (error) => {
+              // Handle Errors here.
+              let errorCode = error.code;
+              let errorMessage = error.message;
+              // The email of the user's account used.
+              let email = error.email;
+              // The firebase.auth.AuthCredential type that was used.
+              let credential = error.credential;
+              // ...
+              console.log(errorMessage);
+            }
+          );
+      });
   }
 
   signOut() {
@@ -54,25 +66,11 @@ export class LoginPage implements OnInit {
         // An error happened.
         console.log("User failed to sign out");
       });
+    this.uDataSvce.deleteSession();
   }
 
   testFireCloud() {
-    var db = firebase.firestore();
-    db.collection("users")
-      .doc(this.user.uid)
-      .set(
-        {
-          username: this.user.displayName,
-          uploads: ["oMYM7XetIgff39WcUAHi"],
-        },
-        { merge: true }
-      )
-      .then(function (docRef) {
-        console.log("Document written with ID: ", docRef);
-      })
-      .catch(function (error) {
-        console.error("Error adding document: ", error);
-      });
+    this.uDataSvce.userUpdate("oMYM7XetIgff39WcUAHi");
   }
 
   ngOnInit() {}
--- src/app/services/user-data.service.ts
+++ src/app/services/user-data.service.ts
@@ -1,10 +1,89 @@
 import { Injectable } from "@angular/core";
+import { NativeStorage } from "@ionic-native/native-storage/ngx";
 
 import * as firebase from "firebase";
+import { UserInfo } from "firebase";
 
 @Injectable({
   providedIn: "root",
 })
 export class UserDataService {
-  constructor() {}
+  private userData: UserInfo;
+  private accessToken: string;
+  private database: any;
+
+  setSession(token: string = "") {
+    this.userData = this.getUser();
+    this.accessToken = token;
+
+    // write to storage
+    this.nativeStorage
+      .setItem("sessionData", {
+        userData: this.userData,
+        accessToken: this.accessToken,
+      })
+      .then(
+        () => console.log("Session Data Saved"),
+        (error) => console.error("Error Saving Session", error)
+      );
+
+    this.nativeStorage.getItem("myitem").then(
+      (data) => console.log(data),
+      (error) => console.error(error)
+    );
+
+    if (!this.database) this.database = firebase.firestore();
+  }
+
+  deleteSession() {
+    this.nativeStorage.remove("sessionData").then(
+      (success) => {
+        console.log("Session data wiped");
+      },
+      (error) => {
+        console.log("Error wiping session data");
+      }
+    );
+  }
+
+  setUser(user: UserInfo) {
+    this.userData = user;
+  }
+
+  getToken() {
+    return this.accessToken;
+  }
+
+  getUser() {
+    if (this.userData) {
+      return this.userData;
+    } else {
+      this.userData = firebase.auth().currentUser;
+      return this.userData;
+    }
+  }
+
+  userUpdate(uploadID: string) {
+    // update Users collection
+    this.database
+      .collection("users")
+      .doc(this.userData.uid)
+      .set(
+        {
+          username: this.userData.displayName,
+          uploads: [uploadID],
+        },
+        { merge: true }
+      )
+      .then(
+        () => {
+          console.log("Success");
+        },
+        (error) => {
+          console.error("Error adding document: ", error);
+        }
+      );
+  }
+
+  constructor(private nativeStorage: NativeStorage) {}
 }
