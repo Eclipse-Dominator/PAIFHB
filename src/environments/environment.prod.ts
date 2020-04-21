658a79a04547605e84bcbd337137178de312d290
WHK <weehekun@gmail.com>
2020-04-21 22:42:35 +0800

    removed stuff
    e

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: "AIzaSyBeJGOS0pKuvN2WhTj_r2fWOtqx9GWVHKs",
-    authDomain: "paifhb.firebaseapp.com",
-    databaseURL: "https://paifhb.firebaseio.com",
-    projectId: "paifhb",
-    storageBucket: "paifhb.appspot.com",
-    messagingSenderId: "1006233367684",
-  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,6 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyBeJGOS0pKuvN2WhTj_r2fWOtqx9GWVHKs",
-    authDomain: "paifhb.firebaseapp.com",
-    databaseURL: "https://paifhb.firebaseio.com",
-    projectId: "paifhb",
-    storageBucket: "paifhb.appspot.com",
-    messagingSenderId: "1006233367684",
-  },
 };
 
 /*
